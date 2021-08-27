7247f8e286db3bf7596a9b78617ec4a946f48748
justinefrange@gmail.com <Stu@Rtlittle123>
2021-08-27 15:13:28 +0800

    Update rawmaterialslist_report.php

--- application/views/report/rawmaterialslist_report.php
+++ application/views/report/rawmaterialslist_report.php
@@ -12,51 +12,110 @@ date_default_timezone_set("Asia/Manila");
 //}
 //return;
 
-class PDFREPORT extends TCPDF {
+class PDFREPORT extends TCPDF
+{
 
 	var $htmlHeader;
 	var $htmlHeaderAddress;
 	var $periodDate;
 
-	public function setHtmlHeader($htmlHeader) {
+	public function setHtmlHeader($htmlHeader)
+	{
 		$this->htmlHeader = $htmlHeader;
 	}
 
-	public function setHtmlHeaderAddress($htmlHeaderAddress) {
+	public function setHtmlHeaderAddress($htmlHeaderAddress)
+	{
 		$this->htmlHeaderAddress = $htmlHeaderAddress;
 	}
 
-	public function setPeriodDate($periodDate) {
+	public function setPeriodDate($periodDate)
+	{
 		$this->htmlPeriodDate = $periodDate;
 	}
 
-	public function Header() {
+	public function Header()
+	{
 		$this->setFont("Helvetica", 'B', 15);
-		$this->writeHTMLCell(0, 0, 0, 4,
-			'RIBSHACK GRILL', 0, 1, 0, '', 'C');
+		$this->writeHTMLCell(
+			0,
+			0,
+			0,
+			4,
+			'RIBSHACK GRILL',
+			0,
+			1,
+			0,
+			'',
+			'C'
+		);
 		$this->setFont("Helvetica", '', 10);
-		$this->writeHTMLCell(0, 0, 0, 10,
-			'OPERATED BY: '.$this->htmlHeader, 0, 1, 0, '', 'C');
-		$this->writeHTMLCell(0, 0, 0, 15,
-			$this->htmlHeaderAddress, 0, 1, 0, '', 'C');
+		$this->writeHTMLCell(
+			0,
+			0,
+			0,
+			10,
+			'OPERATED BY: ' . $this->htmlHeader,
+			0,
+			1,
+			0,
+			'',
+			'C'
+		);
+		$this->writeHTMLCell(
+			0,
+			0,
+			0,
+			15,
+			$this->htmlHeaderAddress,
+			0,
+			1,
+			0,
+			'',
+			'C'
+		);
 		$this->writeHTML("<hr>", true, false, false, false, '');
 	}
 
-	public function Footer() {
+	public function Footer()
+	{
 		// Position at 15 mm from bottom
 		$this->SetY(-15);
 		// Set font
 		$this->SetFont('helvetica', 'I', 7);
 		// Page number
-		$this->Cell(0, 10, 'Page '.$this->getAliasNumPage().' / '.$this->getAliasNbPages(), 0, false, 'L', 0, '', 0, false, 'T', 'M');
+		$this->Cell(0, 10, 'Page ' . $this->getAliasNumPage() . ' / ' . $this->getAliasNbPages(), 0, false, 'L', 0, '', 0, false, 'T', 'M');
 		// Set font
 		$this->SetFont('helvetica', 'I', 8);
-		$this->Cell(0, 10, date("F d, Y h:iA"),
-			0, false, 'R', 0, '', 0, false, 'T', 'M');
-		$this->Cell(0, 17, 'Printed By: '.$_SESSION["rgc_firstname"]." ".$_SESSION["rgc_lastname"],
-			0, false, 'R', 0, '', 0, false, 'T', 'M');
+		$this->Cell(
+			0,
+			10,
+			date("F d, Y h:iA"),
+			0,
+			false,
+			'R',
+			0,
+			'',
+			0,
+			false,
+			'T',
+			'M'
+		);
+		$this->Cell(
+			0,
+			17,
+			'Printed By: ' . $_SESSION["rgc_firstname"] . " " . $_SESSION["rgc_lastname"],
+			0,
+			false,
+			'R',
+			0,
+			'',
+			0,
+			false,
+			'T',
+			'M'
+		);
 	}
-
 }
 // create new PDF document
 $pdf = new PDFREPORT("P", 'mm', 'LETTER', true, 'UTF-8', false);
@@ -79,26 +138,26 @@ $pdf->writeHTMLCell(20, 0, 175, 34, "QTY", 1, 1, 0, 'top', '');
 $y = 42;
 $counter = 1;
 //$meals_possold = 0;
-foreach($rawmat as $ind => $row){
+foreach ($rawmat as $ind => $row) {
 
-    $pdf->SetFont('helvetica', '', 12);
-    $pdf->writeHTMLCell(50, 0, 5, $y, $row["itemcode"], 0, 1, 0, 'top', '');
-    $pdf->writeHTMLCell(100, 0, 55, $y, $row["description"], 0, 1, 0, 'top', '');
-    $pdf->writeHTMLCell(20, 0, 155, $y, $row["uom_abbr"], 0, 1, 0, 'top', '');
-    $pdf->writeHTMLCell(20, 0, 175, $y, " ", 0, 1, 0, 'top', '');
+	$pdf->SetFont('helvetica', '', 12);
+	$pdf->writeHTMLCell(50, 0, 5, $y, $row["itemcode"], 0, 1, 0, 'top', '');
+	$pdf->writeHTMLCell(100, 0, 55, $y, $row["description"], 0, 1, 0, 'top', '');
+	$pdf->writeHTMLCell(20, 0, 155, $y, $row["uom_abbr"], 0, 1, 0, 'top', '');
+	$pdf->writeHTMLCell(20, 0, 175, $y, " ", 0, 1, 0, 'top', '');
 
 
-//	$meals_possold += $row["pos_total"];
-    $y += 7;
-    if($counter >= 20){
-        $pdf->AddPage();
-        $y = 42;
-        $counter = 1;
-    }else{
-        $counter++;
-    }
+	//	$meals_possold += $row["pos_total"];
+	$y += 7;
+	if ($counter >= 30) {
+		$pdf->AddPage();
+		$y = 42;
+		$counter = 1;
+	} else {
+		$counter++;
+	}
 
-    $pdf->writeHTMLCell(200, 0, 3, $y, "<hr />", 0, 1, 0, 'top', '');
+	$pdf->writeHTMLCell(200, 0, 3, $y, "<hr />", 0, 1, 0, 'top', '');
 }
 
 $pdf->SetFont('helvetica', 'B', 15);
